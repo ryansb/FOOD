cc0b016a39f09cb8d861ab3e7323fc56cd363695
Ryan Brown <ryansb@csh.rit.edu>
2011-05-18 18:14:47 -0400

    Up to PEP8 spec now

--- poops.py
+++ poops.py
@@ -9,12 +9,14 @@
 
 from BeautifulSoup import BeautifulSoup, NavigableString
 from sys import version, exit
-import urllib2, time
+from urllib2 import urlopen
+from time import ctime
 
 
 if version.split()[0] >= "3":
     print("This won't run unless you are using python version 2.*")
     exit(1)
+
 
 class Restaurant(object):
     __slots__ = ("link", "contents", "undesireables", "data", "elements",
@@ -28,50 +30,48 @@ class Restaurant(object):
         self.elements = []
         self.new_list = []
         lst = ["BUSINESS SERVICES", "Human Resources", "Digital Den", "Forms",
-		"Napkin Dispenser/Table Tent Policy", "Legal Affairs", "Leadership Staff",
-		"Resources", "All Day", "Information &amp; Technology Services",
-		"RISK MANAGEMENT", "Bookstore", "Italian Entree",
-		"Specialty Wraps-located at Deli", "Bottle Return", "Training",
-		"Institute Audit, Compliance &amp; Advisement", "Weekly Menus",
-		"Directories", "Facilities Management", "Parking &amp; Transportation",
-		"Student Auxiliary Services", "Housing Operations", "Tiger Bucks Program",
-		"Mongo's Grill", "Gordon Field House", "Hours", "Controller",
-		"Mongo's", "Senior Vice President", "Student Financial Services",
-		"About Us/Contacts", "Budget", "Dining Services", "In The News",
-		"Special Delivery", "Global Risk Management Services",
-		"Print &amp; Postal Hub", "Public Safety", "Mexican",
-		"EMPLOYMENT SERVICES", "Center for Professional Development",
-		"FINANCIAL SERVICES", "A-Z Index", "Nutrition", "The Works",
-		"Systems &amp; Technologies", "Hot Meals Under $6",
-		"Information Security", "Career Zone", "Search", "Vending",
-		"Procurement Services", "Ethics &amp; Compliance",
-		"Institutional Research", "Gift Cards",  "Bakery", "Veggie",
-		"Meal Plans and Debit Plans", "Environmental Health &amp; Safety",
-		"Locations", "Emergency Preparedness", "Business Continuity",
-		"F&amp;A LEADERSHIP", "MyRIT", "F&amp;A Home", "Payroll", "Catering",
-		"Tiger Bucks", "Student Employment", "Ancho",
-		"RIT Inn &amp; Conference Center", "Publications", "Accounting",
-		"Restaurant Vendors", "Gracies", "The Ritz Sports Zone",
-		"Brick City Caf\xc3\xa9", "The Commons", "Choices",
-		"The Caf\xc3\xa9 &amp; Market at the Crossroads", "Dinner", "Lunch",
-		"Breakfast", "Better Me", "Same as Lunch",
-		"Global Village Cantina and Grille", "Grill Special", "Bar Items",
-		"Stir Fry", "Specialty Bars", "Pizza Special", "Lunch Combo Meals",
-		"Soup of the Day", "Main Entree", "Deli Special", "Carb Counters",
-		"Vegetarian Entree", "Chef's Special", "Soups", "Dinner Bar",
-		"Lunch Grill Special", "Crossbar", "Daily Specials", "Same as Lunch",
-		"Ancho Grill Rice", "Ancho Grill", "Ancho Grill Special", "Ancho Rice",
-		"Just Veggie"]
+        "Napkin Dispenser/Table Tent Policy", "Legal Affairs", "Leadership Staff",
+        "Resources", "All Day", "Information &amp; Technology Services",
+        "RISK MANAGEMENT", "Bookstore", "Italian Entree",
+        "Specialty Wraps-located at Deli", "Bottle Return", "Training",
+        "Institute Audit, Compliance &amp; Advisement", "Weekly Menus",
+        "Directories", "Facilities Management", "Parking &amp; Transportation",
+        "Student Auxiliary Services", "Housing Operations", "Tiger Bucks Program",
+        "Mongo's Grill", "Gordon Field House", "Hours", "Controller",
+        "Mongo's", "Senior Vice President", "Student Financial Services",
+        "About Us/Contacts", "Budget", "Dining Services", "In The News",
+        "Special Delivery", "Global Risk Management Services",
+        "Print &amp; Postal Hub", "Public Safety", "Mexican",
+        "EMPLOYMENT SERVICES", "Center for Professional Development",
+        "FINANCIAL SERVICES", "A-Z Index", "Nutrition", "The Works",
+        "Systems &amp; Technologies", "Hot Meals Under $6",
+        "Information Security", "Career Zone", "Search", "Vending",
+        "Procurement Services", "Ethics &amp; Compliance",
+        "Institutional Research", "Gift Cards",  "Bakery", "Veggie",
+        "Meal Plans and Debit Plans", "Environmental Health &amp; Safety",
+        "Locations", "Emergency Preparedness", "Business Continuity",
+        "F&amp;A LEADERSHIP", "MyRIT", "F&amp;A Home", "Payroll", "Catering",
+        "Tiger Bucks", "Student Employment", "Ancho",
+        "RIT Inn &amp; Conference Center", "Publications", "Accounting",
+        "Restaurant Vendors", "Gracies", "The Ritz Sports Zone",
+        "Brick City Caf\xc3\xa9", "The Commons", "Choices",
+        "The Caf\xc3\xa9 &amp; Market at the Crossroads", "Dinner", "Lunch",
+        "Breakfast", "Better Me", "Same as Lunch",
+        "Global Village Cantina and Grille", "Grill Special", "Bar Items",
+        "Stir Fry", "Specialty Bars", "Pizza Special", "Lunch Combo Meals",
+        "Soup of the Day", "Main Entree", "Deli Special", "Carb Counters",
+        "Vegetarian Entree", "Chef's Special", "Soups", "Dinner Bar",
+        "Lunch Grill Special", "Crossbar", "Daily Specials", "Same as Lunch",
+        "Ancho Grill Rice", "Ancho Grill", "Ancho Grill Special", "Ancho Rice",
+        "Just Veggie"]
 
         for i in lst:
             self.undesireables.add(i)
 
-
     def scrape_text(self):
-        html = urllib2.urlopen(self.link).read()
+        html = urlopen(self.link).read()
         soup = BeautifulSoup(html)
         self.printText(soup.findAll("li"))
-
 
     def printText(self, tags):
         for tag in tags:
@@ -93,7 +93,7 @@ class Restaurant(object):
         elif not len(self.new_list) > 0:
             self.scrape_text()
             if len(self.contents) == 0:
-				print "There doesn't appear to be anything listed here"
+                print "There doesn't appear to be anything listed here"
             else:
                 for i in self.contents:
                     self.new_list.append(i)
@@ -110,7 +110,9 @@ global_village = Restaurant(
 gracies = Restaurant("http://finweb.rit.edu/diningservices/gracies")
 ritz = Restaurant("http://finweb.rit.edu/diningservices/ritzsportszone")
 
-restaurants = [brick_city_cafe, commons, crossRoads, global_village, gracies, ritz]
+restaurants = [brick_city_cafe, commons, crossRoads, global_village, gracies,
+                ritz]
+
 
 def fix_string(string):
     a = string.replace("&amp;", "&")
@@ -121,40 +123,40 @@ def fix_string(string):
     d = c[0].upper() + c[1:]
     return d
 
+
 def main():
+    header = """
+---------------------------------------------------
+      Parser Of On-campus Preferred Specials
+                      a.k.a.
+        ______ ______ ______ ______  _____
+       |   _  |  __  |  __  |   _  |/ ____|
+       |  |_) | |  | | |  | |  |_) | (___
+       |   ___| |  | | |  | |   ___|\___ \\
+       |  |   | |__| | |__| |  |    ____) |
+       |  |   |      |      |  |   |      |
+       |__|   |______|______|__|   |_____/
 
+      Choose one of the following restuarants
+             to view today's specials
 
-    print "\n"
-    print "---------------------------------------------------"
-    print "      Parser Of On-campus Preferred Specials       "
-    print "                      a.k.a.                       "
-    print "        ______ ______ ______ ______  _____         "
-    print "       |   _  |  __  |  __  |   _  |/ ____|        "
-    print "       |  |_) | |  | | |  | |  |_) | (___          "
-    print "       |   ___| |  | | |  | |   ___|\___ \         "
-    print "       |  |   | |__| | |__| |  |    ____) |        "
-    print "       |  |   |      |      |  |   |      |        "
-    print "       |__|   |______|______|__|   |_____/         "
-    print "\n"
-    print "      Choose one of the following restuarants      "
-    print "             to view today's specials              "
-    print "\n"
-    print "       Today is "+str(time.ctime())
-    print "---------------------------------------------------"
-    print "[1] Brick City Cafe"
-    print "[2] Commons"
-    print "[3] Crossroads"
-    print "[4] Global Village Cantina and Grille"
-    print "[5] Gracies"
-    print "[6] Ritz Sports Zone"
-    print "[q] Quit"
-    print "==================================================="
+       Today is {curtime}
+---------------------------------------------------
+[1] Brick City Cafe"
+[2] Commons"
+[3] Crossroads"
+[4] Global Village Cantina and Grille"
+[5] Gracies"
+[6] Ritz Sports Zone"
+[q] Quit"
+==================================================="""
+    print header.format(curtime=ctime())
     choice = raw_input("Enter your menu choice [1-6 or q]: ")
     try:
         if int(choice) >= 1 and int(choice) <= 6:
-            restaurants[int(choice)-1].construct_list()
+            restaurants[int(choice) - 1].construct_list()
             choice2 = raw_input("Hit the Enter key to go back to the menu, or"
-                + "'q' to quit.")
+                + "'q' to quit. ")
             if choice2 == "q":
                 return
 
@@ -166,7 +168,7 @@ def main():
             while cont != "":
                 cont = raw_input("Please press the Enter key... ")
             main()
-    except:
+    except Exception:
         if choice == 'q':
             return
 
