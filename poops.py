94fec11c375744aaac367705565c516060a44b28
Ryan Brown <ryansb@csh.rit.edu>
2011-05-18 17:53:50 -0400

    Worked towards getting PEP8 compliance

--- poops.py
+++ poops.py
@@ -19,7 +19,7 @@ if version.split()[0] >= "3":
 
 class Restaurant(object):
     __slots__ = ("link", "contents", "undesireables", "data", "elements",
-                 "newList")
+                 "new_list")
 
     def __init__(self, link):
         self.link = link
@@ -27,54 +27,52 @@ class Restaurant(object):
         self.undesireables = set()
         self.data = dict()
         self.elements = []
-        self.newList = []
-        lst = ["BUSINESS SERVICES", "Human Resources", "Digital Den",
-               "Forms", "Napkin Dispenser/Table Tent Policy",
-               "Legal Affairs", "Leadership Staff", "Resources",
-               "All Day", "Information &amp; Technology Services",
-               "RISK MANAGEMENT", "Bookstore", "Italian Entree",
-               "Specialty Wraps-located at Deli", "Bottle Return",
-               "Training", "Institute Audit, Compliance &amp; Advisement",
-               "Weekly Menus", "Directories", "Facilities Management",
-               "Parking &amp; Transportation", "Student Auxiliary Services",
-               "Housing Operations", "Tiger Bucks Program", "Mongo's Grill",
-               "Gordon Field House", "Hours", "Controller", "Mongo's",
-               "Senior Vice President", "Student Financial Services",
-               "About Us/Contacts", "Budget", "Dining Services", "In The News",
-               "Special Delivery", "Global Risk Management Services",
-               "Print &amp; Postal Hub", "Public Safety", "Mexican",
-               "EMPLOYMENT SERVICES", "Center for Professional Development",
-               "FINANCIAL SERVICES", "A-Z Index", "Nutrition", "The Works",
-               "Systems &amp; Technologies", "Hot Meals Under $6",
-               "Information Security", "Career Zone", "Search", "Vending",
-               "Procurement Services", "Ethics &amp; Compliance",
-               "Institutional Research", "Gift Cards",  "Bakery", "Veggie",
-               "Meal Plans and Debit Plans", "Environmental Health &amp; Safety",
-               "Locations", "Emergency Preparedness", "Business Continuity",
-               "F&amp;A LEADERSHIP", "MyRIT", "F&amp;A Home", "Payroll",
-               "Catering", "Tiger Bucks", "Student Employment", "Ancho",
-               "RIT Inn &amp; Conference Center", "Publications", "Accounting",
-               "Restaurant Vendors", "Gracies", "The Ritz Sports Zone",
-               "Brick City Caf\xc3\xa9", "The Commons", "Choices", 
-               "The Caf\xc3\xa9 &amp; Market at the Crossroads", "Dinner",
-               "Lunch", "Breakfast", "Better Me", "Same as Lunch",
-               "Global Village Cantina and Grille", "Grill Special", "Bar Items",
-               "Stir Fry", "Specialty Bars", "Pizza Special", "Lunch Combo Meals",
-               "Soup of the Day", "Main Entree", "Deli Special", "Carb Counters",
-               "Vegetarian Entree", "Chef's Special", "Soups", "Dinner Bar",
-               "Lunch Grill Special", "Crossbar", "Daily Specials", 
-               "Same as Lunch", "Ancho Grill Rice", "Ancho Grill", 
-               "Ancho Grill Special", "Ancho Rice", "Just Veggie"]
-        
+        self.new_list = []
+        lst = ["BUSINESS SERVICES", "Human Resources", "Digital Den", "Forms",
+		"Napkin Dispenser/Table Tent Policy", "Legal Affairs", "Leadership Staff",
+		"Resources", "All Day", "Information &amp; Technology Services",
+		"RISK MANAGEMENT", "Bookstore", "Italian Entree",
+		"Specialty Wraps-located at Deli", "Bottle Return", "Training",
+		"Institute Audit, Compliance &amp; Advisement", "Weekly Menus",
+		"Directories", "Facilities Management", "Parking &amp; Transportation",
+		"Student Auxiliary Services", "Housing Operations", "Tiger Bucks Program",
+		"Mongo's Grill", "Gordon Field House", "Hours", "Controller",
+		"Mongo's", "Senior Vice President", "Student Financial Services",
+		"About Us/Contacts", "Budget", "Dining Services", "In The News",
+		"Special Delivery", "Global Risk Management Services",
+		"Print &amp; Postal Hub", "Public Safety", "Mexican",
+		"EMPLOYMENT SERVICES", "Center for Professional Development",
+		"FINANCIAL SERVICES", "A-Z Index", "Nutrition", "The Works",
+		"Systems &amp; Technologies", "Hot Meals Under $6",
+		"Information Security", "Career Zone", "Search", "Vending",
+		"Procurement Services", "Ethics &amp; Compliance",
+		"Institutional Research", "Gift Cards",  "Bakery", "Veggie",
+		"Meal Plans and Debit Plans", "Environmental Health &amp; Safety",
+		"Locations", "Emergency Preparedness", "Business Continuity",
+		"F&amp;A LEADERSHIP", "MyRIT", "F&amp;A Home", "Payroll", "Catering",
+		"Tiger Bucks", "Student Employment", "Ancho",
+		"RIT Inn &amp; Conference Center", "Publications", "Accounting",
+		"Restaurant Vendors", "Gracies", "The Ritz Sports Zone",
+		"Brick City Caf\xc3\xa9", "The Commons", "Choices",
+		"The Caf\xc3\xa9 &amp; Market at the Crossroads", "Dinner", "Lunch",
+		"Breakfast", "Better Me", "Same as Lunch",
+		"Global Village Cantina and Grille", "Grill Special", "Bar Items",
+		"Stir Fry", "Specialty Bars", "Pizza Special", "Lunch Combo Meals",
+		"Soup of the Day", "Main Entree", "Deli Special", "Carb Counters",
+		"Vegetarian Entree", "Chef's Special", "Soups", "Dinner Bar",
+		"Lunch Grill Special", "Crossbar", "Daily Specials", "Same as Lunch",
+		"Ancho Grill Rice", "Ancho Grill", "Ancho Grill Special", "Ancho Rice",
+		"Just Veggie"]
+
         for i in lst:
             self.undesireables.add(i)
 
 
-    def scrapeText(self):
+    def scrape_text(self):
         html = urllib2.urlopen(self.link).read()
         soup = BeautifulSoup(html)
         self.printText(soup.findAll("li"))
-        
+
 
     def printText(self, tags):
         for tag in tags:
@@ -88,27 +86,28 @@ class Restaurant(object):
             else:
                 self.printText(tag)
 
-    def constructList(self):
+    def construct_list(self):
         print "\n"
-        if len(self.newList) > 0:
-            for h in self.newList:
+        if len(self.new_list) > 0:
+            for h in self.new_list:
                 print h
-        elif not len(self.newList) > 0:
-            self.scrapeText()     
+        elif not len(self.new_list) > 0:
+            self.scrape_text()
             if len(self.contents) == 0:
-                print "There doesn't appear to be anything listed here"
+				print "There doesn't appear to be anything listed here"
             else:
                 for i in self.contents:
-                    self.newList.append(i)
-                self.newList.sort()
-                for j in self.newList:
+                    self.new_list.append(i)
+                self.new_list.sort()
+                for j in self.new_list:
                     print j
         print "\n"
-        
+
 brickCityCafe = Restaurant("http://finweb.rit.edu/diningservices/brickcity")
 commons = Restaurant("http://finweb.rit.edu/diningservices/commons")
 crossRoads = Restaurant("http://finweb.rit.edu/diningservices/crossroads")
-globalVillage = Restaurant("http://finweb.rit.edu/diningservices/gvcantinagrille")
+globalVillage = Restaurant(
+"http://finweb.rit.edu/diningservices/gvcantinagrille")
 gracies = Restaurant("http://finweb.rit.edu/diningservices/gracies")
 ritz = Restaurant("http://finweb.rit.edu/diningservices/ritzsportszone")
 
@@ -154,9 +153,9 @@ def main():
     choice = raw_input("Enter your menu choice [1-6 or q]: ")
     try:
         if int(choice) >= 1 and int(choice) <= 6:
-            restaurants[int(choice)-1].constructList()
-            choice2 = raw_input("Hit the Enter key to go back to the menu, or 'q' to quit. ")
-
+            restaurants[int(choice)-1].construct_list()
+            choice2 = raw_input("Hit the Enter key to go back to the menu, or"
+                + "'q' to quit.")
             if choice2 == "q":
                 return
 
@@ -171,12 +170,12 @@ def main():
     except:
         if choice == 'q':
             return
-    
+
         else:
             print "Oops! Please select choice 1,2,3,4,5,6 or q."
             cont = raw_input("Press the Enter key to continue... ")
             while cont != "":
                 cont = raw_input("Please press the Enter key... ")
             main()
-        
+
 main()
